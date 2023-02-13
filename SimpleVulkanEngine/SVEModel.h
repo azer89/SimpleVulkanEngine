8ec1c7690f5f4c23b5cd5eb20abda0f8d77f1447
Reza Saputra <radhitya@uwaterloo.ca>
2023-02-13 18:10:34 -0500

    fixed uv attribute

--- SimpleVulkanEngine/SVEModel.h
+++ SimpleVulkanEngine/SVEModel.h
@@ -22,7 +22,6 @@ public:
 		glm::vec3 color;
 		glm::vec3 normal{};
 		glm::vec2 uv{};
-		glm::vec2 texCoord{};
 
 		static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 		static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
